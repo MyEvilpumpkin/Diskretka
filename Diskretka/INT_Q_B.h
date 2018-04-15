94be191f4444073e89dd6e1ab9e62c1ef96cada3
MyEvilpumpkin <dima.tomin2010@yandex.ru>
2018-04-15 20:15:36 +0300

    Z-10

--- Diskretka/INT_Q_B.h
+++ Diskretka/INT_Q_B.h
@@ -6,7 +6,7 @@ bool INT_Q_B(Q Numb)
 	Z Temp; // Остаток от деления Числителя на Знаменатель
 	N Module; // Остаток по модулю
 	bool Rez;
-	Temp = MOD_ZZ_Z(*Numb.num, *Numb.denom);
+	Temp = *MOD_ZZ_Z(Numb.num, Numb.denom);
 	Module = *ABS_Z_N(&Temp);
 	Rez = NZER_N_B(&Module);
 	return Rez;
